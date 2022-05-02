5da703d083be2cc829b2f3daae218b7e6dae3258
gqdavin <99968537+gqdavin@users.noreply.github.com>
2022-05-02 21:13:17 +0800

    main.cpp & Start.cpp
    main has only start function
    the path is written according to my computer, need to change later

---
+++ Start.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\header.h>
+using namespace std;
+
+void start(player_info &p){
+	cout<<"Please enter your name: ";
+	cin>>p.name;
+	string input;
+	cout<<"Please allocate your skill points:"<<endl;
+	cout<<"You have "<<p.skill_points<<" skill points at the moment"<<endl;
+	cout<<"You can boost either your 'Attack', 'HP' or 'Spirit'."<<endl;
+	while (p.skill_points!=0){
+		cout<<"Input a number(1: Attack, 2: HP, 3: Spirit): ";
+		cin>>input;
+		if (input=="1"){
+			p.attack+=1;
+			p.skill_points-=1;
+			p.print_ahs();
+		}
+		else if (input=="2"){
+			p.hp+=1;
+			p.skill_points-=1;
+			p.print_ahs();
+		}
+		else if (input=="3"){
+			p.spirit+=1;
+			p.skill_points-=1;
+			p.print_ahs();
+		}
+		else
+		cout<<"Please enter a valid number."<<endl;
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\start.cpp>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\header.h>
+
+using namespace std;
+
+
+int main(){
+	player_info p;
+	start(p);
+	p.print_all_status();
+}
