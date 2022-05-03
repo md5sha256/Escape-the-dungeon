b9ccbb152b75ed891e7fbfda478418db96765b24
gqdavin <99968537+gqdavin@users.noreply.github.com>
2022-05-03 21:15:09 +0800

    Add files via upload

---
+++ src/battle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\header.h>
+using namespace std;
+
+vector<enemy> generate_enemies(){
+			vector<enemy> enemy_generated;//the list of enemy generated
+			srand(time(NULL));
+			int num_enemy=(rand()%5)+1;
+			for (int i=0;i<num_enemy;i++){
+				enemy temp;
+				srand(time(NULL)+i);
+				temp.enemy_generation();//call for the helper function to generate one enemy
+				enemy_generated.push_back(temp);//append this enemy to the enemy generated list
+			}
+			return enemy_generated;
+}
+
+
+
+void battle(){
+	
+}
