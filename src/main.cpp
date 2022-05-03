3bae6fd0e5035faec5e732a16e6014566466b10b
gqdavin <99968537+gqdavin@users.noreply.github.com>
2022-05-03 16:31:44 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include "playerinfo.hpp"
-#include "start.cpp"
-#include <algorithm>
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\start.cpp>
+#include<C:\Users\Davin\Desktop\Study\Uni stuffs\1340\Project\header.h>
 
 using namespace std;
 
@@ -10,5 +10,23 @@ using namespace std;
 int main(){
 	player_info p;
 	start(p);
-	p.print_all_status();
+	while(p.position!=10){
+		if(p.path[p.position]==0){//when battle is encountered
+			
+		}
+		else if(p.path[p.position]==1){//when campfire is encountered
+			
+		}
+		else if(p.path[p.position]==2){//when shop is encountered
+			
+		}
+		else if(p.path[p.position]==3){//when random event is encountered
+			
+		}
+		else if(p.path[p.position]==4){//when boss is encountered
+			
+		}
+		
+		p.position+=1;
+	}
 }
