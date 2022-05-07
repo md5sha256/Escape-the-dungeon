989d2e6c8f5036bf9d5d456053275fabca872738
gqdavin <99968537+gqdavin@users.noreply.github.com>
2022-05-07 09:16:31 +0800

    Update start.cpp
    fixed infinity loop while enters an invalid path number,
    path generation is more clear

--- src/start.cpp
+++ src/start.cpp
@@ -21,46 +21,46 @@ Player* initPlayer() {
     std::cout << "Path 1:" << std::endl;
     for (int i = 0; i < 10; i++) {
         if (g.path1[i] == 0)
-            std::cout << "Battle >>";
+            std::cout << "Battle >> ";
         if (g.path1[i] == 1)
-            std::cout << "Campfire >>";
+            std::cout << "Campfire >> ";
         if (g.path1[i] == 2)
-            std::cout << "Shop >>";
+            std::cout << "Shop >> ";
         if (g.path1[i] == 3)
-            std::cout << "Event >>";
+            std::cout << "Event >> ";
         if (g.path1[i] == 4)
-            std::cout << "Boss>>Escape" << std::endl;
+            std::cout << "Boss >> Escape" << std::endl;
     }
     std::cout << "Path 2:" << std::endl;
     for (int i = 0; i < 10; i++) {
         if (g.path2[i] == 0)
-            std::cout << "Battle >>";
+            std::cout << "Battle >> ";
         if (g.path2[i] == 1)
-            std::cout << "Campfire >>";
+            std::cout << "Campfire >> ";
         if (g.path2[i] == 2)
-            std::cout << "Shop >>";
+            std::cout << "Shop >> ";
         if (g.path2[i] == 3)
-            std::cout << "Event >>";
+            std::cout << "Event >> ";
         if (g.path2[i] == 4)
             std::cout << "Boss >> Escape" << std::endl;
     }
     std::cout << "Path 3:" << std::endl;
     for (int i = 0; i < 10; i++) {
         if (g.path3[i] == 0)
-            std::cout << "Battle >>";
+            std::cout << "Battle >> ";
         if (g.path3[i] == 1)
-            std::cout << "Campfire >>";
+            std::cout << "Campfire >> ";
         if (g.path3[i] == 2)
-            std::cout << "Shop >>";
+            std::cout << "Shop >> ";
         if (g.path3[i] == 3)
-            std::cout << "Event >>";
+            std::cout << "Event >> ";
         if (g.path3[i] == 4)
             std::cout << "Boss >> Escape" << std::endl;
     }
     std::cout << "Enter a path number: ";
-    std::cin >> input;
     std::vector<int> playerPath;
     while (true) {
+        std::cin >> input;
         if (input == "1")
             playerPath = g.path1;
         else if (input == "2")
